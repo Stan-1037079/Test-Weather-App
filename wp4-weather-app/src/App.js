6d10724796f1499a2ff49fc3da02eef8a239a446
Stan-1037079 <113016783+Stan-1037079@users.noreply.github.com>
2023-06-13 17:44:29 +0200

    Html en Api gekoppeld

--- wp4-weather-app/src/App.js
+++ wp4-weather-app/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=amsterdam&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
+  
   return (
     <div className="App">
-      <h1>Test test</h1>
-    </div>
+      <div className="container">
+        <div className="top">
+          <div className="locatie">
+            <p>Amsterdam</p>
+        </div>
+        <div className="temp">
+          <h1>30°C</h1>
+      </div>
+      <div className='beschrijving'>
+        <p>Cloudy</p>
+      </div>
+      </div>
+      <div className="bottom"></div>
+      </div>
+    </div>  
   );
 }
 
